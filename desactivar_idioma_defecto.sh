170503933eec2b4ba6244d8c710b6d1441961348
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-09-19 17:09:24 +0200

    Update desactivar_idioma_defecto.sh

--- desactivar_idioma_defecto.sh
+++ desactivar_idioma_defecto.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
-# Desactivar el idioma por defecto
-    echo ""
-    echo ""
+# Desactivar el idioma por defecto en APT
 
-if [ -f "/etc/apt/apt.conf.d/30desdability-lang" ];then
+echo ""
+echo ""
 
+if [ -f "/etc/apt/apt.conf.d/30desdability-lang" ]; then
     echo "-------------------------------------------"
-    echo "* El idioma por defecto ya esta desactivado *"
+    echo "* El idioma por defecto ya está desactivado *"
     echo "-------------------------------------------"
-
     echo ""
     echo ""
 else
-
-    echo -e "Acquire::Languages \"none\"; Acquire::IndexTargets::deb::Contents-deb::DefaultEnabled \"false\";\\n" > /etc/apt/apt.conf.d/30desdability-lang
+    echo "Acquire::Languages \"none\";" > /etc/apt/apt.conf.d/30desdability-lang
+    echo "Acquire::IndexTargets::deb::Contents-deb::DefaultEnabled \"false\";" >> /etc/apt/apt.conf.d/30desdability-lang
 
     echo "------------------------------------------"
     echo "* El idioma por defecto se ha desactivado *"
     echo "------------------------------------------"
-
     echo ""
     echo ""
-    echo "Reinicia proxmox para aplicar el cambio"
+    echo "Reinicia Proxmox para aplicar el cambio"
     echo ""
     echo ""
 fi
