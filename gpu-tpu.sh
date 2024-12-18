e748faaf33cf6f7172791c3013abd22a4fb206bf
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-12-18 19:30:35 +0100

    Update gpu-tpu.sh

--- gpu-tpu.sh
+++ gpu-tpu.sh
@@ -20,6 +20,14 @@ msg_error() {
     echo -e " ${RD}[ERROR] $1${CL}"
 }
 
+# Validar que el contenedor seleccionado es válido
+validate_container_id() {
+    if [ -z "$CONTAINER_ID" ]; then
+        msg_error "ID del contenedor no definido. Asegúrate de seleccionar un contenedor primero."
+        exit 1
+    fi
+}
+
 # Verificar la versión de Proxmox
 validate_pve_version() {
     if ! pveversion | grep -Eq "pve-manager/(8\\.[0-9]+)"; then
@@ -31,16 +39,22 @@ validate_pve_version() {
 
 # Selección del contenedor LXC
 select_container() {
-    CONTAINERS=$(pct list | awk 'NR>1 {print $1, $3}')
-    CONTAINER_ID=$(whiptail --title "Seleccionar Contenedor" --menu "Selecciona el contenedor LXC al que deseas añadir recursos:" 15 60 5 $(echo $CONTAINERS | xargs -n2) 3>&1 1>&2 2>&3)
-
-    if [ -z "$CONTAINER_ID" ]; then
-        msg_error "No se seleccionó ningún contenedor."
+    CONTAINERS=$(pct list | awk 'NR>1 {print $1, $3}' | xargs -n2)
+    if [ -z "$CONTAINERS" ]; then
+        msg_error "No hay contenedores disponibles en Proxmox."
         exit 1
     fi
 
-    if ! pct status "$CONTAINER_ID" &>/dev/null; then
-        msg_error "No se encontró el contenedor con ID $CONTAINER_ID."
+    CONTAINER_ID=$(whiptail --title "Seleccionar Contenedor" \
+        --menu "Selecciona el contenedor LXC:" 15 60 5 $CONTAINERS 3>&1 1>&2 2>&3)
+
+    if [ -z "$CONTAINER_ID" ]; then
+        msg_error "No se seleccionó ningún contenedor. Saliendo."
+        exit 1
+    fi
+
+    if ! pct list | awk 'NR>1 {print $1}' | grep -qw "$CONTAINER_ID"; then
+        msg_error "El contenedor con ID $CONTAINER_ID no existe. Saliendo."
         exit 1
     fi
 
@@ -49,7 +63,13 @@ select_container() {
 
 # Validar y cambiar a privilegiado si es necesario
 ensure_privileged_container() {
+    validate_container_id
     CONFIG_FILE="/etc/pve/lxc/${CONTAINER_ID}.conf"
+    if [ ! -f "$CONFIG_FILE" ]; then
+        msg_error "Archivo de configuración del contenedor $CONTAINER_ID no encontrado."
+        exit 1
+    fi
+
     if grep -q "^unprivileged: 1" "$CONFIG_FILE"; then
         msg_info "El contenedor es no privilegiado. Cambiando a privilegiado..."
         sed -i "s/^unprivileged: 1/unprivileged: 0/" "$CONFIG_FILE"
@@ -68,7 +88,6 @@ ensure_privileged_container() {
 verify_and_add_repos() {
     msg_info "Verificando y configurando repositorios necesarios en el host..."
 
-    # Repositorio de Proxmox no suscripción
     if ! grep -q "pve-no-subscription" /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
         echo "deb http://download.proxmox.com/debian/pve $(lsb_release -sc) pve-no-subscription" | tee /etc/apt/sources.list.d/pve-no-subscription.list
         msg_ok "Repositorio pve-no-subscription añadido."
@@ -76,7 +95,6 @@ verify_and_add_repos() {
         msg_ok "Repositorio pve-no-subscription ya configurado."
     fi
 
-    # Repositorio de firmware non-free
     if ! grep -q "non-free-firmware" /etc/apt/sources.list; then
         echo "deb http://deb.debian.org/debian $(lsb_release -sc) main contrib non-free-firmware
 deb http://deb.debian.org/debian $(lsb_release -sc)-updates main contrib non-free-firmware
@@ -90,37 +108,51 @@ deb http://security.debian.org/debian-security $(lsb_release -sc)-security main 
     msg_ok "Repositorios verificados y actualizados."
 }
 
-# Configurar repositorio de Coral TPU en el contenedor
-add_coral_repos() {
-    msg_info "Verificando repositorios de Coral TPU en el contenedor..."
-    pct exec "$CONTAINER_ID" -- bash -c "
-    if ! grep -q 'coral-edgetpu-stable' /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
-        echo 'deb https://packages.cloud.google.com/apt coral-edgetpu-stable main' | tee /etc/apt/sources.list.d/coral-edgetpu.list
-        curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/coral-edgetpu.gpg
-        msg_ok 'Repositorio de Coral TPU añadido.'
-    else
-        msg_ok 'Repositorio de Coral TPU ya configurado.'
+# Configurar Coral TPU en el contenedor
+configure_lxc_for_coral() {
+    ensure_privileged_container
+    CONFIG_FILE="/etc/pve/lxc/${CONTAINER_ID}.conf"
+
+    # Verificar y agregar configuraciones solo si no existen
+    if ! grep -q "c 189:* rwm" "$CONFIG_FILE"; then
+        echo "lxc.cgroup2.devices.allow: c 189:* rwm # Coral USB" >> "$CONFIG_FILE"
+        echo "lxc.mount.entry: /dev/bus/usb dev/bus/usb none bind,optional,create=dir" >> "$CONFIG_FILE"
     fi
-    apt-get update &>/dev/null
-    "
+
+    if ! grep -q "c 29:0 rwm # Coral M.2" "$CONFIG_FILE"; then
+    #    echo "lxc.cgroup2.devices.allow: c 29:0 rwm # Coral M.2" >> "$CONFIG_FILE"
+        echo "lxc.mount.entry: /dev/apex_0 dev/apex_0 none bind,optional,create=file" >> "$CONFIG_FILE"
+    else
+        msg_info "Permisos para Coral M.2 ya configurados."
+    fi
+
+    msg_ok "Configuración de Coral TPU (USB y M.2) añadida al contenedor $CONTAINER_ID."
 }
 
 # Configurar iGPU en el contenedor
 configure_lxc_for_igpu() {
     ensure_privileged_container
     CONFIG_FILE="/etc/pve/lxc/${CONTAINER_ID}.conf"
-    sed -i '/^lxc\.cgroup2\.devices\.allow: c 226:/d' "$CONFIG_FILE"
-    sed -i '/^lxc\.mount\.entry: \/dev\/dri/d' "$CONFIG_FILE"
 
-    cat <<EOF >> "$CONFIG_FILE"
-features: nesting=1
-lxc.cgroup2.devices.allow: c 226:0 rwm # iGPU
-lxc.cgroup2.devices.allow: c 226:128 rwm # iGPU
-lxc.cgroup2.devices.allow: c 29:0 rwm # Framebuffer
-lxc.mount.entry: /dev/fb0 dev/fb0 none bind,optional,create=file
-lxc.mount.entry: /dev/dri dev/dri none bind,optional,create=dir
-lxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file
-EOF
+    # Verificar y agregar configuraciones solo si no existen
+    if ! grep -q "features: nesting=1" "$CONFIG_FILE"; then
+        echo "features: nesting=1" >> "$CONFIG_FILE"
+    fi
+
+    if ! grep -q "c 226:0 rwm" "$CONFIG_FILE"; then
+        echo "lxc.cgroup2.devices.allow: c 226:0 rwm # iGPU" >> "$CONFIG_FILE"
+        echo "lxc.cgroup2.devices.allow: c 226:128 rwm # iGPU" >> "$CONFIG_FILE"
+        echo "lxc.mount.entry: /dev/dri dev/dri none bind,optional,create=dir" >> "$CONFIG_FILE"
+        echo "lxc.mount.entry: /dev/dri/renderD128 dev/dri/renderD128 none bind,optional,create=file" >> "$CONFIG_FILE"
+    fi
+
+    if ! grep -q "c 29:0 rwm # Framebuffer" "$CONFIG_FILE"; then
+        echo "lxc.cgroup2.devices.allow: c 29:0 rwm # Framebuffer" >> "$CONFIG_FILE"
+    fi
+
+    if ! grep -q "lxc.mount.entry: /dev/fb0" "$CONFIG_FILE"; then
+        echo "lxc.mount.entry: /dev/fb0 dev/fb0 none bind,optional,create=file" >> "$CONFIG_FILE"
+    fi
 
     msg_ok "Configuración de iGPU añadida al contenedor $CONTAINER_ID."
 }
@@ -129,14 +161,16 @@ EOF
 install_igpu_in_container() {
     msg_info "Instalando controladores de iGPU dentro del contenedor..."
     pct start "$CONTAINER_ID"
-    VERBOSE_OPTION=$(whiptail --title "Modo Verbose" --yesno "¿Deseas habilitar el modo detallado para la instalación de controladores?" 8 58 && echo "" || echo "silent")
     pct exec "$CONTAINER_ID" -- bash -c "
-    silent() { \"\$@\" >/dev/null 2>&1; } && $VERBOSE_OPTION apt-get update && $VERBOSE_OPTION apt-get install -y va-driver-all ocl-icd-libopencl1 intel-opencl-icd vainfo intel-gpu-tools && chgrp video /dev/dri && chmod 755 /dev/dri && $VERBOSE_OPTION adduser root video && $VERBOSE_OPTION adduser root render
+    apt-get update && \
+    apt-get install -y va-driver-all ocl-icd-libopencl1 intel-opencl-icd vainfo intel-gpu-tools && \
+    chgrp video /dev/dri && chmod 755 /dev/dri && \
+    adduser root video && adduser root render
     "
     msg_ok "Controladores de iGPU instalados dentro del contenedor."
 }
 
-# Instalar Coral TPU en el host
+# Instalar controladores Coral TPU en el host
 install_coral_host() {
     msg_info "Instalando controladores de Coral TPU en el host..."
     verify_and_add_repos
@@ -150,48 +184,23 @@ install_coral_host() {
     msg_ok "Controladores de Coral TPU instalados en el host."
 }
 
-# Configurar Coral TPU en el contenedor
-configure_lxc_for_coral() {
-    ensure_privileged_container
-    CONFIG_FILE="/etc/pve/lxc/${CONTAINER_ID}.conf"
-
-    # Configurar dispositivos Coral TPU sin verificar su detección
-    cat <<EOF >> "$CONFIG_FILE"
-lxc.cgroup2.devices.allow: c 189:* rwm # Coral USB
-lxc.mount.entry: /dev/bus/usb dev/bus/usb none bind,optional,create=dir
-lxc.cgroup2.devices.allow: c 29:0 rwm # Coral M.2
-lxc.mount.entry: /dev/apex_0 dev/apex_0 none bind,optional,create=file
-EOF
-
-    msg_ok "Configuración de Coral TPU (USB y M.2) añadida al contenedor $CONTAINER_ID."
-}
-
 # Instalar controladores Coral TPU en el contenedor
 install_coral_in_container() {
     msg_info "Detectando dispositivos Coral TPU dentro del contenedor..."
     CORAL_M2=$(lspci | grep -i "Global Unichip")
 
     if [[ -n "$CORAL_M2" ]]; then
-        msg_info "Se detectó un Coral M.2. Preguntando por la versión de controladores a instalar..."
         DRIVER_OPTION=$(whiptail --title "Seleccionar versión de controladores" \
             --menu "Elige la versión de controladores para Coral M.2:\n\nPrecaución: El modo máximo genera más calor." 15 60 2 \
             1 "libedgetpu1-std (rendimiento estándar)" \
             2 "libedgetpu1-max (máximo rendimiento)" 3>&1 1>&2 2>&3)
 
         case "$DRIVER_OPTION" in
-            1)
-                DRIVER_PACKAGE="libedgetpu1-std"
-                ;;
-            2)
-                DRIVER_PACKAGE="libedgetpu1-max"
-                ;;
-            *)
-                msg_error "Opción no válida. Instalando libedgetpu1-std por defecto."
-                DRIVER_PACKAGE="libedgetpu1-std"
-                ;;
+            1) DRIVER_PACKAGE="libedgetpu1-std" ;;
+            2) DRIVER_PACKAGE="libedgetpu1-max" ;;
+            *) DRIVER_PACKAGE="libedgetpu1-std" ;;
         esac
     else
-        msg_info "No se detectó un Coral M.2. Instalando libedgetpu1-std por defecto."
         DRIVER_PACKAGE="libedgetpu1-std"
     fi
 
@@ -208,11 +217,11 @@ install_coral_in_container() {
 # Menú principal
 main_menu() {
     OPTIONS=(
-        1 "Añadir aceleración gráfica iGPU"
-        2 "Añadir Coral TPU (incluye instalación en host y contenedor)"
+        1 "Añadir aceleración gráfica HW iGPU"
+        2 "Añadir Coral TPU + HW iGPU"
     )
 
-    OPTION=$(whiptail --title "Menú Principal" --menu "Selecciona los recursos a añadir al contenedor:" 15 60 2 "${OPTIONS[@]}" 3>&1 1>&2 2>&3)
+    OPTION=$(whiptail --title "Menú Principal" --menu "Selecciona los recursos a añadir a un LXC:" 15 60 2 "${OPTIONS[@]}" 3>&1 1>&2 2>&3)
 
     case "$OPTION" in
         1)
@@ -223,6 +232,8 @@ main_menu() {
             install_coral_host
             configure_lxc_for_coral
             install_coral_in_container
+            configure_lxc_for_igpu
+            install_igpu_in_container
             ;;
         *)
             msg_error "Opción no válida. Saliendo."
@@ -233,4 +244,7 @@ main_menu() {
     msg_ok "Configuración completada."
 }
 
+# Iniciar
+validate_pve_version
+select_container
 main_menu
